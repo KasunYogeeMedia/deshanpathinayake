0926f4c8036c67159bbfa035410d07981e118f3d
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-01-30 10:16:08 +0530

    link fix

--- dashboard/navheader.php
+++ dashboard/navheader.php
@@ -7,7 +7,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
         <div class="nav-header">
-            <a href="index.html" class="brand-logo">
+            <a href="index.php" class="brand-logo">
                 <img class="logo-abbr" src="images/logo-white.png" alt="">
                 <img class="logo-compact" src="images/logo-text-white.png" alt="">
                 <img class="brand-title" src="images/logo-text-white.png" alt="">
